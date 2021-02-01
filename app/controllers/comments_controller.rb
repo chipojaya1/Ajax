701ec5cd8495ee76ad1204b00d6cc5fac6f85d9b
Chipo Jaya <chipojaya@gmail.com>
2021-02-01 12:00:11 +0000

    editingcomment posting function and forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,21 @@
 class CommentsController < ApplicationController
+# An action to save and post a comment.
   def create
+    # Search Blog from the parameter value and build it as comments associated with Blog.
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.build(comment_params)
+    # Change format according to client request
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to blog_path(@blog) }
+      else
+        format.html { redirect_to blog_path(@blog), notice: 'Couldn't post...' }
+      end
+    end
+  end
+  private
+  # Strong parameters
+  def comment_params
+    params.require(:comment).permit(:blog_id, :content)
   end
 end
